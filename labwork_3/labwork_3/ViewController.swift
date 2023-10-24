c9764f2be1b5ee0350b17e3d6f434a33c44e6607
Mekriy <98518826+Mekriy@users.noreply.github.com>
2023-10-24 18:49:13 +0300

    new changes

--- labwork_3/labwork_3/ViewController.swift
+++ labwork_3/labwork_3/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var color1: UIColor = UIColor.red
+    var count: Int = 0
+    var winstreak: Int = 0
+    var cardIndex: Int? = nil
+    var WinCards: [Bool] = [false, false, false, false, false, false]
+    @IBOutlet weak var WinningStreakLabel: UILabel!
+    
     @IBAction func buttonSwapCards(_ sender: Any) {
         guard let button = sender as? UIButton,
               let index = buttons.firstIndex(of: button) else {
@@ -25,11 +31,54 @@ class ViewController: UIViewController {
         
         let a = topCardViews[index]
         let b = bottomCardViews[index]
+        if WinCards[index] == true{
+            return
+        }
+        if !a.isHidden {
+            UIView.transition(from: b, to: a, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            UIView.transition(from: a, to: b, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
         
-        UIView.transition(from: a, to: b, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        a.isHidden.toggle()
+        b.isHidden.toggle()
+        	
         
-        topCardViews[index].isHidden.toggle()
-        bottomCardViews[index].isHidden.toggle()
+        if count == 0 {
+            color1 = b.backgroundColor!
+            count += 1
+            cardIndex = index
+        }
+        else {
+            if cardIndex == index{
+                count = 0
+                return
+            }
+            else {
+                if !topCardViews[cardIndex!].isHidden {
+                    color1 = UIColor.white
+                }
+                if color1 == b.backgroundColor{
+                    winstreak += 1
+                    WinningStreakLabel.text = "Winning streak = \(winstreak)"
+                    WinCards[cardIndex!] = true
+                    WinCards[index] = true
+                    count = 0
+                }
+                else{
+                    winstreak = 0
+                    WinningStreakLabel.text = "Winning streak = \(winstreak)"
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                        UIView.transition(from: b, to: a, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+                        let c = self.topCardViews[self.cardIndex!]
+                        let d = self.bottomCardViews[self.cardIndex!]
+                        UIView.transition(from: d, to: c, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+                        self.count = 0
+                    }
+                }
+                
+            }
+        }
+        
     }
-    
 }
